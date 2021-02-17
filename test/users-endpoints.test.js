db35467f96ee3ea6b148fa1f82aad9cd98fcac28
James Clifford <jamesclifford7@gmail.com>
2021-02-17 12:20:07 -0600

    updated test db url for tests --all tests passing again

--- test/users-endpoints.test.js
+++ test/users-endpoints.test.js
@@ -13,7 +13,7 @@ let db;
 before(() => {
     db = knex({
         client: 'pg', 
-        connection: process.env.TEST_DB_URL
+        connection: process.env.TEST_DATABASE_URL
     })
     app.set('db', db)
 }); 
